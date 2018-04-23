2a879c99eb9f754f4580f5bf7bc9c9f287965d25
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-23 19:55:24 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,6 @@
 class CreateArtists < ActiveRecord::Migration
   def change
-    add_column :artists, :grade, :integer 
+    add_column :artists, :grade, :integer
     add column :artists, :birthdate, :string
    end
   end
